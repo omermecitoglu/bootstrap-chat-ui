4305e36ad28ef3a459db22717f1386ce5659c2f9
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-11-09 08:13:52 +0700

    add Font Awesome

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,13 @@
+import { faHandPeace } from "@fortawesome/free-solid-svg-icons/faHandPeace";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import Container from "react-bootstrap/Container";
 
 const App = () => {
   return (
-    <Container>Bootstrap</Container>
+    <Container>
+      Bootstrap with Font Awesome <FontAwesomeIcon icon={faHandPeace} />
+    </Container>
   );
 };
 
