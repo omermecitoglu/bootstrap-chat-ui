21673484cd123fd3966cff626020a0a79db1fe69
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-11-09 07:52:21 +0700

    Initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,32 @@
+import path from "node:path";
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-themes",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  webpackFinal: async (conf) => {
+    return {
+      ...conf,
+      resolve: {
+        ...conf.resolve,
+        alias: {
+          "~": path.resolve(__dirname, "../src/"),
+        },
+      },
+    };
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+};
+export default config;
---
+++ src/components/App/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="myapp">Typescript React Storybook Template</div>
+  );
+};
+
+export default App;
