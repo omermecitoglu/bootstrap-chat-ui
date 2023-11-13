452ac70bbb04cd22ad6b0a3b6cc22ef94b8e5693
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-11-13 11:41:57 +0700

    build

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ export type IMessage = {
   roomId: string,
   authorId: string,
   timestamp: number,
-  status: "new" | "created" | "delivered" | "read",
+  status: "dummy" | "new" | "created" | "delivered" | "read",
 };
 export type ChatProps = {
   originUserId: string,
